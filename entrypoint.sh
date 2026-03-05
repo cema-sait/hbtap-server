1ad6a211d886c593572dfec6e7d33b89ac712590
ayahaustine <austinjuniour4@gmail.com>
2026-03-05 15:56:22 +0300

    feat: use PostgreSQL in backup setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
+echo "Making migrations for users..."
+python manage.py makemigrations users --noinput
+
+echo "Migrating users..."
+python manage.py migrate users --noinput
+
+echo "Making migrations for members..."
+python manage.py makemigrations members --noinput
+
+echo "Migrating members..."
+python manage.py migrate members --noinput
+
+echo "Running remaining migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
--- hta/settings.py
+++ hta/settings.py
@@ -147,23 +147,15 @@ CSRF_TRUSTED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'hbtap'),
+        'USER': os.environ.get('POSTGRES_USER', 'hbtap'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'hbtap'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    }
 }
-
-
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.postgresql',
-#       'NAME': '*****',
-#       'USER': '****',
-#        'PASSWORD': '*******',
-#        'HOST': 'localhost',
-#        'PORT': 5432,
-#    }
-#}
 
 
 
