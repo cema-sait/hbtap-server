f0304a75a503e17ee1be9e559950f4010b63e233
ayahaustine <austinjuniour4@gmail.com>
2026-03-17 10:11:50 +0300

    feat: move to postgres

--- hta/settings.py
+++ hta/settings.py
@@ -139,24 +139,24 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB', 'hbtap'),
-#         'USER': os.environ.get('POSTGRES_USER', 'hbtap'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'hbtap'),
-#         'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-#         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-#     }
-# }
-
-# use sqlite for backup
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'hbtap'),
+        'USER': os.environ.get('POSTGRES_USER', 'hbtap'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'hbtap'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
+
+# # use sqlite for backup
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
